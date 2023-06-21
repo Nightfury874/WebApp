0c85ddb0c5a60945b79710d6f0b8faa1a799d590
Nighfury874 <jibaba874@gmail.com>
2023-06-21 19:20:52 +0530

    added @RestController and @GetMapping

---
+++ WebApp/src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.springboot;
+//import GetMapping
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index(){
+        return "Hi1 Whats up?";
+    }
+    
+}
